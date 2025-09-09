d1b1490c204753a1930c8efdde073146ca62cc80
nandika-k <nkarnik5@outlook.com>
2025-09-09 12:05:08 -0400

    assignment complete

---
+++ HW1/target/debug/HW1.d
@@ -0,0 +1,1 @@
+C:\Users\nkarn\Code\YWCC307\HW1\target\debug\HW1.exe: C:\Users\nkarn\Code\YWCC307\HW1\src\main.rs
---
+++ HW1/target/debug/deps/HW1.d
@@ -0,0 +1,5 @@
+C:\Users\nkarn\Code\YWCC307\HW1\target\debug\deps\HW1.d: src\main.rs
+
+C:\Users\nkarn\Code\YWCC307\HW1\target\debug\deps\HW1.exe: src\main.rs
+
+src\main.rs:
